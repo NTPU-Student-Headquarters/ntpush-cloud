cb80e9b4d6f612bfe7ce2e56b16817863277d1e0
Zhen Huang <193303856+zhenhuang-tw@users.noreply.github.com>
2026-04-20 23:18:37 +0800

    chore(site): 把網站簡稱再改得簡單一些

--- config/site.config.ts
+++ config/site.config.ts
@@ -1,7 +1,7 @@
 // config/site.config.ts
 export const siteConfig = {
   name: 'NTPUSCS Legislative Hub',
-  shortName: 'ntpuscs LegiHub',
+  shortName: 'LegiHub',
   orgName: '國立臺北大學學生議會（三峽校區）',
   orgNameEn: 'NTPU Student Congress (Sanxia)',
   orgUrl: 'https://cloud.ntpusu.org/',
